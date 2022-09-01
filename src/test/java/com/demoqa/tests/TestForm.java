4a74cc1eed53a466722327528b7df09006979eac
Viktor_Rus <vaurusov@mts.ru>
2022-09-01 16:45:04 +0300

    done home work

--- src/test/java/com/demoqa/tests/TestForm.java
+++ src/test/java/com/demoqa/tests/TestForm.java
@@ -2,6 +2,7 @@ package com.demoqa.tests;
 
 
 import com.demoqa.pages.RegistrationFormPageObject;
+import static io.qameta.allure.Allure.step;
 import org.junit.jupiter.api.Test;
 
 public class TestForm extends TestBase {
@@ -9,31 +10,36 @@ public class TestForm extends TestBase {
 
     @Test
     void testForm() {
-        registrationFormPageObject.openPage().
-                setFirstName("Ivan").
-                setLastName("Ivanov").
-                setUserEmail("email@email.com").
-                setGender("Male").
-                setUserNumber("9374364782").
-                setBirthDate("29", "May", "1993").
-                setSubjectsInput("English").
-                setHobbies("Sports").
-                setUploadPicture("src/test/resources/selenide-logo-big.png").
-                setCurrentAddress("My Address Soviet Union").
-                setState("Haryana").
-                setCity("Karnal").
-                clickSubmit();
+        step("Заполняем форму регистрации", ()-> {
+            registrationFormPageObject.openPage().
+                    setFirstName("Ivan").
+                    setLastName("Ivanov").
+                    setUserEmail("email@email.com").
+                    setGender("Male").
+                    setUserNumber("9374364782").
+                    setBirthDate("29", "May", "1993").
+                    setSubjectsInput("English").
+                    setHobbies("Sports").
+                    setUploadPicture("src/test/resources/selenide-logo-big.png").
+                    setCurrentAddress("My Address Soviet Union").
+                    setState("Haryana").
+                    setCity("Karnal").
+                    clickSubmit();
+        });
 
-        registrationFormPageObject.checkResultTableVisible().
-                checkRegistrationSubmittingFormTests("Student Name", "Ivan Ivanov").
-                checkRegistrationSubmittingFormTests("Student Email", "email@email.com").
-                checkRegistrationSubmittingFormTests("Gender", "Male").
-                checkRegistrationSubmittingFormTests("Mobile", "9374364782").
-                checkRegistrationSubmittingFormTests("Date of Birth", "29 May,1993").
-                checkRegistrationSubmittingFormTests("Subjects", "English").
-                checkRegistrationSubmittingFormTests("Hobbies", "Sports").
-                checkRegistrationSubmittingFormTests("Picture", "selenide-logo-big.png").
-                checkRegistrationSubmittingFormTests("Address", "My Address Soviet Union").
-                checkRegistrationSubmittingFormTests("State and City", "Haryana Karnal");
+        step("Проверяем форму", ()-> {
+            registrationFormPageObject.checkResultTableVisible().
+                    checkRegistrationSubmittingFormTests("Student Name", "Ivan Ivanov").
+                    checkRegistrationSubmittingFormTests("Student Email", "email@email.com").
+                    checkRegistrationSubmittingFormTests("Gender", "Male").
+                    checkRegistrationSubmittingFormTests("Mobile", "9374364782").
+                    checkRegistrationSubmittingFormTests("Date of Birth", "29 May,1993").
+                    checkRegistrationSubmittingFormTests("Subjects", "English").
+                    checkRegistrationSubmittingFormTests("Hobbies", "Sports").
+                    checkRegistrationSubmittingFormTests("Picture", "selenide-logo-big.png").
+                    checkRegistrationSubmittingFormTests("Address", "My Address Soviet Union").
+                    checkRegistrationSubmittingFormTests("State and City", "Haryana Karnal");
+        });
+
     }
 }
